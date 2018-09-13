8f9eaac709e2f62ba8639f592ec8d7ef8d43eaa6
indiaprince <21600767@handong.edu>
2018-09-14 07:50:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/indiaprince/codingclinic/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.indiaprince.codingclinic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);   //xml 잡아줌  lyaout
+    }
+}
+
+
+
+
+//버튼하고 이미지 추가 버튼 누르면 이벤트로 원래 있는 글 바꾸기
