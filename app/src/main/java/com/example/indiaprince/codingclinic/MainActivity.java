ba1909defb4baac95dfbf0326edcafe6436eed17
indiaprince <21600767@handong.edu>
2018-09-17 07:17:54 +0900

    button changes text

--- app/src/main/java/com/example/indiaprince/codingclinic/MainActivity.java
+++ app/src/main/java/com/example/indiaprince/codingclinic/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.indiaprince.codingclinic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int k=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);   //xml 잡아줌  lyaout
+        setContentView(R.layout.activity_main);   //xml 잡아줌  layout
+
+        final TextView text=findViewById(R.id.text);
+        Button btn1=findViewById(R.id.button);
+        btn1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                if(k%3==0) {
+                    text.setText("HI");
+                }
+                else if(k%3==1){
+                    text.setText("My Name Is");
+                }
+                else if(k%3==2){
+                    text.setText("Hyeon Su Han");
+                }
+                k++;
+            }
+        });
     }
 }
 
